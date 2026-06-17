f5910601fb88e780940e84854fef5628a29febf3
Ayush Yadav <52788335+ayush31yadav@users.noreply.github.com>
2026-06-17 14:26:36 +0530

    improved readability and got the branch ready for merge

--- modules/Comparator.v
+++ modules/Comparator.v
@@ -1,16 +1,13 @@
 module Comparator(
-    input [31:0] A,
-    input [31:0] B,
-
-    output LS,          //high if lesser signed
-    output LU,          //high if lesser unsigned
-    output E            //high if equal
-    );
+    input  wire [31:0] A, B,
+    output wire LS,          //high if lesser signed
+    output wire LU,          //high if lesser unsigned
+    output wire EQ           //high if equal
+);
     
     
-    assign E = (A==B);
+    assign EQ = (A==B);
     assign LU = (A<B);
-    assign LS = ( $signed(A) < $signed(B));
-    
+    assign LS = ($signed(A) < $signed(B));
     
 endmodule
